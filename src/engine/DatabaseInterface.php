f6050facf7debcd1f278322a4ee19a642f97f268
Nicholas English <30304307+obozaxy@users.noreply.github.com>
2018-02-06 15:33:53 -0500

    Update DatabaseInterface.php

--- src/engine/DatabaseInterface.php
+++ src/engine/DatabaseInterface.php
@@ -1,1 +1,58 @@
+<?php
+declare(strict_types=1);
+/**
+ * This file is a part of secure-php-login-system.
+ *
+ * @author Akbar Hashmi (Owner/Developer)           <me@akbarhashmi.com>.
+ * @author Nicholas English (Contributor/Developer) <nenglish0820@outlook.com>.
+ *
+ * @link    <https://github.com/akbarhashmi/Secure-PHP-Login-System> Github repository.
+ * @license <https://github.com/akbarhashmi/Secure-PHP-Login-System/blob/master/LICENSE> MIT license.
+ */
 
+namespace Akbarhashmi\Engine;
+
+/**
+ * DatabaseInterface.
+ */
+interface DatabaseInterface
+{
+
+    /**
+     * Create a new PDO database connection.
+     * 
+     * @param string $driver       The driver to use.
+     * @param array  $dnsDetails   The dns details.
+     * @param bool   $debugEnabled Should we run debugging.
+     *
+     * @throws UnexpectedValueException If driver selected does not exist
+     *                                  or is not supported.
+     *
+     * @return void.
+     */
+    function __construct(string $driver, array $dnsDetails, bool $debugEnabled);
+    
+    /**
+     * Set the debugging state.
+     *
+     * @param bool $debug Should we run debugging.
+     *
+     * @return void.
+     */
+    private function debug(bool $debug): void;
+    
+    /**
+     * Hide the internal contents from view.
+     *
+     * @return array|[] Return an empty array.
+     */
+    public function __debugInfo();
+    
+    /**
+     * Prevent serialization.
+     *
+     * @return array[] Return an empty array.
+     */
+    public function __sleep(): array;
+    
+}
